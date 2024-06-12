475aa2e1dbc220e6d1f98c8ce06bb2219db4dedc
Ajay o s <ajayosakhub@gmail.com>
2024-06-12 17:45:29 +0000

    update with backup

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Fab, Popover, Typography } from '@mui/material';
+import { Box, Fab, Grid, Popover, Typography } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
 import ChatBox from './ChatBox';
 
@@ -17,64 +17,63 @@ function App() {
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
+  const an = window.navigator.userAgent.includes("Android") ? true : false;
   return (
-    <Box>
-      <Box
-        position="fixed"
-        alignContent={'center'}
-        left="10%"
-        transform="translateX(-50%)"
-        zIndex={1000}
-      >
-        <Typography
-          variant="h1"
+    <Grid container spacing={2} justifyContent="center" alignItems="center" height="100vh">
+      <Grid item xs={12} md={6} lg={4}>
+        <Box position="fixed" left="10%" transform="translateX(-50%)" zIndex={1000}>
+          <Typography
+            variant="h1"
+            style={{
+              color: 'green',
+              padding: '10px 20px',
+              borderRadius: '8px',
+              boxShadow: '0 10px 8px rgba(255, 0, 0, 0.2)',
+              fontFamily: 'Arial, sans-serif',
+            }}
+          >
+             BOT DEMO
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid item xs={12} md={5} lg={4}>
+        <Fab
+          color="primary"
+          aria-label="bot"
+          onClick={handleClick}
           style={{
-            color: 'green',
-            padding: '10px 20px',
-            borderRadius: '8px',
-            boxShadow: '0 10px 8px rgba(255, 0, 0, 0.2)',
-            fontFamily: 'Arial, sans-serif',
+            position: 'fixed',
+            bottom: an ? 20 : 80,
+            right: an ? 20: 50,
           }}
         >
-          BANK ASSISTANCE BOT
-        </Typography>
-      </Box>
-
-      <Fab
-        color="primary"
-        aria-label="bot"
-        onClick={handleClick}
-        style={{
-          position: 'fixed',
-          bottom: 20,
-          right: 20,
-        }}
-      >
-        <ChatIcon />
-      </Fab>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        sx={{
-          '& .MuiPaper-root': {
-            width: '430px', // Set your desired width
-            height: '510px', // Set your desired height
-          },
-        }}
-      >
-        <ChatBox />
-      </Popover>
-    </Box>
+          <ChatIcon />
+        </Fab>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          sx={{
+            '& .MuiPaper-root': {
+              width: '430px', // Set your desired width
+              height: '510px', // Set your desired height
+            },
+          }}
+        >
+          <ChatBox />
+        </Popover>
+      </Grid>
+    </Grid>
   );
 }
 
--- web/src/ChatBox.jsx
+++ web/src/ChatBox.jsx
@@ -1,36 +1,52 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Box, Button, TextField, Typography, Grid, Avatar, CircularProgress } from '@mui/material';
-import PersonIcon from '@mui/icons-material/Person';
-import SmartToyIcon from '@mui/icons-material/SmartToy';
+import React, { useState, useRef, useEffect } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Grid,
+  Avatar,
+  CircularProgress,
+} from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 
 const ChatBox = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [messages, setMessages] = useState(
+    JSON.parse(localStorage.getItem("chatMessages")) || []
+  );
+  const [inputValue, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
   const [buttons, setButtons] = useState([
-    { name: 'Debit', res: 'debit' },
-    { name: 'Credit', res: 'credit' },
-    { name: 'Info', res: 'info' },
+    { name: "Debit", res: "debit" },
+    { name: "Credit", res: "credit" },
+    { name: "Info", res: "info" },
   ]);
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
 
   const handleSendMessage = async (content) => {
-    setMessages([...messages, { type: 'user', content }]);
-    setLoading(true);
-    setInputValue('');
-    setButtons([]);
-
     try {
-      const response = await fetch('/chat', {
-        method: 'POST',
+      // localStorage.setItem("chatMessages", JSON.stringify([{ type: "user", content: content }]));
+      setLoading(true);
+
+      localStorage.setItem(
+        "chatMessages",
+        JSON.stringify([...messages, { type: "user", content: content }])
+      );
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "user", content: content },
+      ]);
+      const response = await fetch("/chat", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ message: content }),
       });
@@ -41,22 +57,46 @@ const ChatBox = () => {
       if (result.messages) {
         setMessages((prevMessages) => [
           ...prevMessages,
-          { type: 'bot', content: result.messages },
+          { type: "bot", content: result.messages },
         ]);
+        const newBotMessage = { type: "bot", content: result.messages };
+        localStorage.setItem(
+          "chatMessages",
+          JSON.stringify([...messages, newBotMessage])
+        );
       }
 
       if (result.buttons) {
         setButtons(result.buttons);
+        setLoading(false);
+      } else {
+        setButtons([]);
       }
     } catch (error) {
+      localStorage.setItem(
+        "chatMessages",
+        JSON.stringify([...messages, { type: "user", content: content }])
+      );
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "user", content: content },
+      ]);
+      console.error("Error sending message:", error);
+      const botResponse = `Sorry, I'm having trouble understanding you. Please try again.`;
+      const newBotMessage = { type: "bot", content: botResponse };
+      setMessages((prevMessages) => [...prevMessages, newBotMessage]);
       setLoading(false);
-      console.error('Error sending message:', error);
+      localStorage.setItem(
+        "chatMessages",
+        JSON.stringify([...messages, newBotMessage])
+      );
     }
   };
 
   const handleInputSend = () => {
-    if (inputValue.trim() !== '') {
+    if (inputValue.trim() !== "") {
       handleSendMessage(inputValue.trim());
+      setInputValue("");
     }
   };
 
@@ -65,28 +105,28 @@ const ChatBox = () => {
   };
 
   return (
-    <Box p={2} width={400}>
+    <Box p={2} width={1} maxWidth={400}>
       <Typography variant="h6" gutterBottom>
         Bank Assistance for You
       </Typography>
-      <Box height={300} mb={2} overflow="auto" border={1} p={1}>
+      <Box height={310} mb={2} overflow="auto" border={1} p={1}>
         {messages.map((msg, index) => (
           <Box
-            key={index}
+            key={index+''+Math.floor(Math.random() * 100)}
             display="flex"
-            flexDirection={msg.type === 'user' ? 'row-reverse' : 'row'}
+            flexDirection={msg.type === "user" ? "row-reverse" : "row"}
             alignItems="center"
             mb={1}
           >
             <Avatar>
-              {msg.type === 'user' ? <PersonIcon /> : <SmartToyIcon />}
+              {msg.type === "user" ? <PersonIcon /> : <SmartToyIcon />}
             </Avatar>
             <Box
-              ml={msg.type === 'user' ? 1 : 2}
-              mr={msg.type === 'user' ? 2 : 1}
+              ml={msg.type === "user" ? 1 : 2}
+              mr={msg.type === "user" ? 2 : 1}
               p={1}
-              bgcolor={msg.type === 'user' ? 'primary.main' : 'grey.300'}
-              color={msg.type === 'user' ? 'white' : 'black'}
+              bgcolor={msg.type === "user" ? "primary.main" : "grey.300"}
+              color={msg.type === "user" ? "white" : "black"}
               borderRadius={2}
             >
               <Typography>{msg.content}</Typography>
@@ -103,7 +143,7 @@ const ChatBox = () => {
       {buttons.length > 0 ? (
         <Grid container spacing={1}>
           {buttons.map((button, index) => (
-            <Grid item key={index} xs={buttons.length > 2 ? 4 : 6}> {/* Adjust column width based on the number of buttons */}
+            <Grid item key={index} xs={6} sm={4} md={3}>
               <Button
                 fullWidth
                 variant="contained"
@@ -128,7 +168,7 @@ const ChatBox = () => {
             onClick={handleInputSend}
             variant="contained"
             color="primary"
-            style={{ marginLeft: '8px' }}
+            style={{ marginLeft: "8px" }}
             disabled={loading}
           >
             Send
